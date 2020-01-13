535f1e7c6192388997c304c89d94317c8e09b50d
David Adi Nugroho <davidadi216@gmail.com>
2020-01-13 13:55:22 +0700

    update feb 2020

--- parser.py
+++ parser.py
@@ -92,7 +92,7 @@ def main():
 
     for id, name in cities.items():
         print('processing ' + name)
-        write_file(name, get_adzans(id, '01', '2020'))
+        write_file(name, get_adzans(id, '02', '2020'))
 
 
 if __name__ == "__main__":
